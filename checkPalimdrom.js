4edd7f6acab7435440947d82e1719dc7b29c7789
Sumi'Stake <sumi.sparkal@gmail.com>
2025-09-24 10:43:36 +0530

    Add files via upload

---
+++ checkPalimdrom.js
@@ -0,0 +1,12 @@
+//check given number is palindrom or not
+function palinNo(num){
+    let originalNo=num;
+    let rev=0;
+    for(;num>0;num=Math.floor(num/10)){
+        rev=(rev*10)+(num%10);
+    }if(originalNo===rev){
+        return`${originalNo} is palindrom`;
+    }else{  return`${originalNo} is NOT palindrom`;}
+}console.log(palinNo(121));
+console.log(palinNo(211));
+console.log(palinNo(191));
