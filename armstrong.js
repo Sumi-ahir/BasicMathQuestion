3d52e7c852a5e8476d69fb656caa2a90233d301b
Sumi'Stake <sumi.sparkal@gmail.com>
2025-09-26 09:21:40 +0530

    Add files via upload

---
+++ armstrong.js
@@ -0,0 +1,35 @@
+//find armstrong num
+function armstrongNo(n){
+    let sum=0;
+    let temp=n;
+    let degits=n.toString().length;
+
+    while(temp!==0 || temp>0){
+      let degit=temp%10;
+      sum+=Math.pow(degit,degits);
+      temp=Math.floor(temp/10);
+
+    }return sum===n;
+
+}console.log(armstrongNo(123));
+console.log(armstrongNo(153)); 
+console.log(armstrongNo(9474));
+
+
+//MORE OPTIMISE WAY
+function armstrongNo(n){
+    let sum=0;
+    let temp=n;
+    let degits=n.toString().length;
+
+    while(temp!==0 || temp>0){
+      let degit=temp%10;
+      sum+=Math.pow(degit,degits);
+      if(sum>n) return false;
+      temp=Math.floor(temp/10);
+
+    }return sum===n;
+
+}console.log(armstrongNo(123));
+console.log(armstrongNo(153)); 
+console.log(armstrongNo(9474));
